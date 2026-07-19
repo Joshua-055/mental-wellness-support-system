3aafccb1ac62b3a88585d10dfbc4809d9cbc2264
Joshua-055 <joshua05tham@gmail.com>
2026-07-20 00:13:20 +0800

    Initial project setup

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$pageTitle = "Home";
+include "includes/header.php";
+?>
+
+<div class="container py-5">
+
+    <h1 class="text-primary">
+        Bootstrap Successfully Loaded!
+    </h1>
+
+    <p class="lead">
+        Mental Wellness Support System
+    </p>
+
+    <button class="btn btn-success">
+        Test Button
+    </button>
+
+    <button class="btn btn-danger">
+        Delete
+    </button>
+
+</div>
+
+<?php include "includes/footer.php"; ?>
